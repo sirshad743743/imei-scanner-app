248e4d9a00517c76b94907f3bdd80eb7115809c4
sirshad743743 <s.irshad743@gmail.com>
2025-10-26 00:54:06 +0530

    Delete imei_scanner_app/imei_scanner_app directory

--- imei_scanner_app/imei_scanner_app/static/config.js
+++
@@ -1,7 +0,0 @@
-// Configure how to save records
-// 'client'  -> front-end will POST directly to Google Apps Script URL
-// 'server'  -> front-end will POST to /save (Flask), which forwards to Apps Script URL from config.json/env
-window.SAVE_VIA = 'server';
-
-// Place your Google Apps Script Web App URL here when SAVE_VIA = 'client'
-window.GOOGLE_SHEETS_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycby7QuYGdGvol1CeoDptZ7de77QQlo0APTYNtC0IMyAvsYFpcce8cFuKkglZW63TEOrGJw/exec';
